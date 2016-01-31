e5ffce08e634425bd17e0cf2ffaa2c701aeff0fc
dartraiden <wowemuh@gmail.com>
2016-01-31 04:26:46 +0300

    https support

--- answer_to.meta.js
+++ answer_to.meta.js
@@ -2,10 +2,13 @@
 // @name			Answer to...
 // @description		Shows the comment for which this comment is an answer
 // @namespace		Habrahabr
-// @version        	1.3.6
+// @version        	1.3.7
 // @include			http://geektimes.ru/*
+// @include			https://geektimes.ru/*
 // @include			http://habrahabr.ru/*
+// @include			https://habrahabr.ru/*
 // @include			http://megamozg.ru/*
+// @include			https://megamozg.ru/*
 // @updateURL	    https://github.com/Inversion-des/Habrahabr--Answer-to/raw/master/answer_to.meta.js
 // @downloadURL     https://github.com/Inversion-des/Habrahabr--Answer-to/raw/master/answer_to.user.js
 // @supportURL	    https://github.com/Inversion-des/Habrahabr--Answer-to/issues
