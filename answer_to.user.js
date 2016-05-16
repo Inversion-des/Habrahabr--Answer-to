100fbd946f1947c28f70ba8fefa35fd7b119e32a
dartraiden <wowemuh@gmail.com>
2016-05-16 22:07:39 +0300

    remove megamozg

--- answer_to.user.js
+++ answer_to.user.js
@@ -8,9 +8,8 @@
 // @version        	1.3.8
 // @include			https://geektimes.ru/*
 // @include			https://habrahabr.ru/*
-// @include			https://megamozg.ru/*
 // @updateURL	    https://github.com/Inversion-des/Habrahabr--Answer-to/raw/master/answer_to.meta.js
-// @downloadURL	https://github.com/Inversion-des/Habrahabr--Answer-to/raw/master/answer_to.user.js
+// @downloadURL		https://github.com/Inversion-des/Habrahabr--Answer-to/raw/master/answer_to.user.js
 // @supportURL	    https://github.com/Inversion-des/Habrahabr--Answer-to/issues
 // ==/UserScript==
 
