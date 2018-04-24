15f2fe10ebb5d84640c250e1529bfd61b0e48743
dartraiden <wowemuh@gmail.com>
2018-04-24 19:22:45 +0300

    correct domain names

--- answer_to.meta.js
+++ answer_to.meta.js
@@ -2,9 +2,9 @@
 // @name			Answer to...
 // @description		Shows the comment for which this comment is an answer
 // @namespace		Habrahabr
-// @version        	1.3.9
-// @include			https://geektimes.ru/*
-// @include			https://habrahabr.ru/*
+// @version        	1.3.10
+// @include			https://geektimes.com/*
+// @include			https://habr.com/*
 // @updateURL	    https://github.com/Inversion-des/Habrahabr--Answer-to/raw/master/answer_to.meta.js
 // @downloadURL		https://github.com/Inversion-des/Habrahabr--Answer-to/raw/master/answer_to.user.js
 // @supportURL	    https://github.com/Inversion-des/Habrahabr--Answer-to/issues
